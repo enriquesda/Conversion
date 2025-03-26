e2499976e083815b9b07507cc99b793fbb320fbe
enriquesda <enriquealbuera60@gmail.com>
2025-03-26 17:46:54 +0100

    selecRuta

--- src/Menu.java
+++ src/Menu.java
@@ -1,17 +1,68 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Menu {
     private String ruta;
-    private ArrayList<String> archivos;
+    private ArrayList <File> archivos;
     private Archivo selec;
+
+    public Menu(){
+        this.ruta="......";
+        this.archivos= new ArrayList<>();
+        this.selec=null;
+    }
+    
+
+    public String getRuta() {
+        return ruta;
+    }
+
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+
+    public ArrayList<File> getArchivos() {
+        return archivos;
+    }
+
+
+    public void setArchivos(ArrayList<File> archivos) {
+        this.archivos = archivos;
+    }
+
+
+    public Archivo getSelec() {
+        return selec;
+    }
+
+
+    public void setSelec(Archivo selec) {
+        this.selec = selec;
+    }
 
 
     /**
      * comprueba si la ruta selecioda existe y si es asi la establece y guarda los archivos que tiene
      * @param r
      */
-    public void selecRuta(String r){
-
+    public boolean selecRuta(String r){
+        File fichero = new File(r);
+        if (fichero.exists() && fichero.isDirectory()) {
+            this.ruta=r;
+            String[] contenido = fichero.list();
+            if (archivos != null && contenido.length > 0) {
+            for (String archivo : contenido) {
+                        File archi = new File(archivo);
+                        archivos.add(archi);
+                    }
+                }
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
@@ -29,7 +80,7 @@ public class Menu {
      * @param formato
      * @param ruta
      */
-    
+
     public void conversion(String formato,String nombre){
 
     }
@@ -41,6 +92,14 @@ public class Menu {
 
     public String imprimirMenu(){
         return " ";
+    }
+
+    public String imprimirArchivos(){
+        StringBuilder dev= new StringBuilder();
+        for (File file : archivos) {
+            dev.append(file.getName()).append("\n");
+        }
+        return dev.toString();
     }
 
 
