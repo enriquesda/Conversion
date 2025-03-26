2f1eacf65bd918e52f05bb469b40702e43f7e634
Enrique <enriquealbuera60@gmail.com>
2025-03-26 12:23:02 +0100

    ESCIRBIRJSON

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 
+import java.io.IOException;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
@@ -8,7 +10,17 @@ public class App {
         System.err.println("Su extension es :" + ar.getFormato());
         System.out.println("Contenido de archivo:");
         ar.imprimirContenido();
-        ar.escribirCSV("src/archivos/copiacoches.csv");
+
+        try {
+            ar.escribirCSV("src/archivos/copiacoches.csv");
+        } catch (IOException e) {
+            System.err.println("No se pudo escribir el archivo: " + e.getMessage());
+        }
+        try {
+            ar.escribirJSON("src/archivos/copiacoches.json");
+        } catch (IOException e) {
+            System.err.println("No se pudo escribir el archivo: " + e.getMessage());
+        }
 
     }
 }
