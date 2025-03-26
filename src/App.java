59cfe96a245d2af62cc38b32ffb32e9c9b459186
Enrique <enriquealbuera60@gmail.com>
2025-03-26 11:37:51 +0100

    escribirCSV

--- src/App.java
+++ src/App.java
@@ -1,22 +1,14 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-         try (BufferedReader br = new BufferedReader(new FileReader("src/archivos/coches.xml"))) {
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                String[] palabras = linea.replaceAll("[<>/]", " ").trim().toLowerCase().split("\\s+");
-                for (String n : palabras) {
-                    System.out.print(n+" ");
-                }
-                System.out.println("\n");
-            }
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-  
+        Archivo ar = new Archivo("src/archivos/coches.xml");
+        System.out.println("La ruta es : " + ar.getRuta());
+        System.err.println("Su extension es :" + ar.getFormato());
+        System.out.println("Contenido de archivo:");
+        ar.imprimirContenido();
+        ar.escribirCSV("src/archivos/copiacoches.csv");
+
     }
 }
--- src/Archivo.java
+++ src/Archivo.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -7,13 +8,14 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 public class Archivo {
+
     private String ruta;
     private ArrayList<LinkedHashMap<String, String>> archivo;
     private int formato; // 0->csv 1->json 2->xml
 
     public Archivo(String ruta) {
         this.ruta = ruta;
-        String[] aux = ruta.split(".");
+        String[] aux = ruta.split("\\.");
         switch (aux[aux.length - 1]) {
             case "csv":
                 formato = 0;
@@ -28,26 +30,55 @@ public class Archivo {
 
                 break;
         }
+        this.archivo = lecturaXML(ruta);
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public ArrayList<LinkedHashMap<String, String>> getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(ArrayList<LinkedHashMap<String, String>> archivo) {
+        this.archivo = archivo;
+    }
+
+    public int getFormato() {
+        return formato;
+    }
+
+    public void setFormato(int formato) {
+        this.formato = formato;
     }
 
     public ArrayList<LinkedHashMap<String, String>> lecturaXML(String r) {
         ArrayList<LinkedHashMap<String, String>> lista = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("src/archivos/coches.xml"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(r))) {
             String linea;
-            boolean cierre=false;
+            boolean cierre = false;
             while ((linea = br.readLine()) != null) {
                 String[] palabras = linea.replaceAll("[<>/]", " ").trim().toLowerCase().split("\\s+");
-                if(!cierre){
-                    if (palabras.length>1) {
-                        cierre=true;
-                        LinkedHashMap<String, String> a = new LinkedHashMap<>(); 
+                if (!cierre) {
+                    if (palabras.length > 1) {
+                        cierre = true;
+                        LinkedHashMap<String, String> a = new LinkedHashMap<>();
                         a.put(palabras[0], palabras[1]);
                         lista.add(a);
                     }
-                }else{
-
+                } else {
+                    if (palabras.length > 1) {
+                        LinkedHashMap<String, String> ultimoMapa = lista.get(lista.size() - 1);
+                        ultimoMapa.put(palabras[0], palabras[1]);
+                    } else {
+                        cierre = false;
+                    }
                 }
-
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
@@ -74,16 +105,56 @@ public class Archivo {
         return xml;
     }
 
-    public Archivo escribirJSON() {
+    public Archivo escribirJSON(String r) {
         Archivo json = null;
 
         return json;
     }
 
-    public Archivo escribirCSV() {
-        Archivo csv = null;
+    public void escribirCSV(String r) throws IOException {
 
-        return csv;
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(r))) {
+
+            StringBuilder linea = new StringBuilder();
+
+            for (LinkedHashMap<String, String> mapa : archivo) {
+                for (String clave : mapa.keySet()) {
+                    String aniadir = Character.toUpperCase(clave.charAt(0)) + clave.substring(1);
+                    linea.append(aniadir).append(",");
+                }
+            }
+
+            if (linea.length() > 0) {
+                linea.setLength(linea.length() - 1);
+            }
+
+            bw.write(linea.toString());
+            bw.write("\n");
+
+            for (LinkedHashMap<String, String> mapa : archivo) {
+                StringBuilder lineacon = new StringBuilder();
+                for (String clave : mapa.keySet()) {
+                    String aniadir = Character.toUpperCase(mapa.get(clave).charAt(0)) + mapa.get(clave).substring(1);
+                    lineacon.append(aniadir).append(",");
+                }
+                if (lineacon.length() > 0) {
+                    lineacon.setLength(lineacon.length() - 1);
+                }
+                bw.write(lineacon.toString());
+                bw.write("\n");
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
     }
 
+    public void imprimirContenido() {
+        for (LinkedHashMap<String, String> mapa : archivo) {
+            System.out.println("---- Nuevo coche ----");
+            for (String clave : mapa.keySet()) {
+                System.out.println(clave + " : " + mapa.get(clave));
+            }
+        }
+    }
 }
