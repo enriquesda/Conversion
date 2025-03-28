93066eeed2a594b0edbfdfbb909fe01ba9a88f6b
Enrique <enriquealbuera60@gmail.com>
2025-03-28 08:38:45 +0100

    corre

--- src/App.java
+++ src/App.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class App {
 
-    public static Scanner sc= new Scanner(System.in);
+    public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
         Menu menu = new Menu();
         boolean funcionando = true;
         do {
-            
+
             System.out.println("Selecciona una opción del menú");
             System.out.println("1: Seleccionar carpeta");
             System.out.println("2: Lectura de archivos");
@@ -19,39 +19,39 @@ public class App {
 
             int numero = Integer.parseInt(sc.nextLine());
             switch (numero) {
-                case 1 -> ejercicio1();
-                case 2 -> ejercicio2();
-                case 3 -> convertirEn();
+                case 1 ->
+                    ejercicio1();
+                case 2 ->
+                    ejercicio2();
+                case 3 ->
+                    convertirEn();
 
-                default -> funcionando = false;
+                default ->
+                    funcionando = false;
             }
         } while (funcionando);
         sc.close();
 
-
-
-
-        String path="Conversion/src/archivos";
-        Menu menu = new Menu();
+        String path = "Conversion/src/archivos";
         if (menu.selecRuta(path)) {
             System.out.println("Ruta valida");
             System.out.println(menu.getRuta());
             System.out.println(menu.imprimirArchivos());
-            if(menu.selecFichero("coches.xml")){
+            if (menu.selecFichero("coches.xml")) {
                 Archivo a = menu.getSelec();
                 a.imprimirContenido();
             }
-            
-        }else{
+
+        } else {
             System.out.println("Ruta no valida");
         }
-        
+
     }
 
     public static void convertirEn() throws IOException {
         boolean funcionando = true;
         do {
-            
+
             System.out.println("A qué formato quieres convertir?");
             System.out.println("1: Convertir a JSON");
             System.out.println("2: Convertir a XML");
@@ -61,15 +61,15 @@ public class App {
             int numero = Integer.parseInt(sc.nextLine());
             switch (numero) {
                 case 1:
-                
 
-                break;
+                    break;
                 case 2:
-                break;
+                    break;
                 case 3:
-                break;
+                    break;
 
-                default -> funcionando = false;
+                default ->
+                    funcionando = false;
             }
         } while (funcionando);
         sc.close();
--- src/Menu.java
+++ src/Menu.java
@@ -1,65 +1,56 @@
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.io.File;
-import java.util.Scanner;
-import java.io.IOException;
 
 public class Menu {
 
     private String ruta;
-    private ArrayList <File> archivos;
+    private ArrayList<File> archivos;
     private Archivo selec;
 
-    public Menu(){
-        this.ruta=".........";
-        this.archivos= new ArrayList<>();
-        this.selec=null;
+    public Menu() {
+        this.ruta = ".........";
+        this.archivos = new ArrayList<>();
+        this.selec = null;
     }
-    
 
     public String getRuta() {
         return ruta;
     }
 
-
     public void setRuta(String ruta) {
         this.ruta = ruta;
     }
-
 
     public ArrayList<File> getArchivos() {
         return archivos;
     }
 
-
     public void setArchivos(ArrayList<File> archivos) {
         this.archivos = archivos;
     }
-
 
     public Archivo getSelec() {
         return selec;
     }
 
-
     public void setSelec(Archivo selec) {
         this.selec = selec;
     }
 
-
     /**
-     * comprueba si la ruta selecioda existe y si es asi la establece y guarda los
-     * archivos que tiene
-     * 
+     * comprueba si la ruta selecioda existe y si es asi la establece y guarda
+     * los archivos que tiene
+     *
      * @param r
      */
     public boolean selecRuta(String r) {
         File directorio = new File(r);
         if (directorio.exists() && directorio.isDirectory()) {
-            this.ruta = directorio.getAbsolutePath(); 
+            this.ruta = directorio.getAbsolutePath();
             this.archivos.clear();
-            
+
             File[] contenido = directorio.listFiles();
             if (contenido != null) {
                 for (File archivo : contenido) {
@@ -76,29 +67,28 @@ public class Menu {
     /**
      * Comprueba si la ruta del fichero es correcta(pertenece al directorio que
      * estamos) y lo guarda como archivo selec
-     * 
+     *
      * @param r
      */
     public boolean selecFichero(String r) {
         if (archivos == null || archivos.isEmpty()) {
             return false;
         }
-    
+
         for (File file : archivos) {
             if (file.getName().equals(r)) {
                 this.selec = new Archivo(file.getAbsolutePath());
                 return true;
             }
         }
-        return false; 
+        return false;
     }
 
     /**
-     * 
+     *
      * @param formato
      * @param ruta
      */
-
     public void conversion(String formato, String nombre) { //CORREGIR, NO ME MOLA NADA
         if (this.selec != null) {
             try {
@@ -121,29 +111,25 @@ public class Menu {
     //REPE¿?¿?¿ SEGURAMENTE BORRARS
     public boolean comprobarRuta(String r) {
         File ruta = new File(r);
-        if(ruta.exists()){
+        if (ruta.exists()) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
-        
+
     }
 
     public String imprimirMenu() {
         return " ";
     }
 
-    public String imprimirArchivos(){
-        StringBuilder dev= new StringBuilder();
+    public String imprimirArchivos() {
+        StringBuilder dev = new StringBuilder();
         for (File file : archivos) {
             dev.append(file.getName()).append("\n");
-            
 
         }
         return dev.toString();
     }
 
-
-    
 }
